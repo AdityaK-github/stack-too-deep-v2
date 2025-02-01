ace7ac0b0ca2826c21ef547a966844cf1e769381
Aditya Satish Karwa <aditya@adityas-mac-2.local>
2025-02-01 14:51:33 +0530

    modify contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -6,8 +6,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFT is ERC721URIStorage, Ownable {
     uint256 private _tokenIds = 0;
+    
+    event Mint(address indexed recipient, uint256 indexed tokenId, string uri);
+    mapping (address => uint[]) ownerToTokens;
+    mapping (address => string[]) ownerToTokenURI;
 
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) Ownable(msg.sender) {}
+    constructor() ERC721("MyNFTCollection", "MNFT") Ownable(msg.sender) {}
 
     function mint(address recipient, string memory uri) public onlyOwner returns (uint256) {
         require(recipient != address(0), "Cannot mint to zero address");
@@ -16,15 +20,56 @@ contract NFT is ERC721URIStorage, Ownable {
         uint256 newItemId = _tokenIds;
         _tokenIds++;
         
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, uri);
+        _mint(recipient, newItemId); 
+        _setTokenURI(newItemId, uri); 
+        
+        ownerToTokens[recipient].push(newItemId);
+        ownerToTokenURI[recipient].push(uri);
+        emit Mint(recipient, newItemId, uri);
         
         return newItemId;
-    }   
+    }
+
+
+    function getOwnerNftUri(address owner) public view returns (string[] memory) {
+        return ownerToTokenURI[owner];
+    }
 
     function transferNFT(address to, uint256 tokenId) public {
         require(ownerOf(tokenId) == msg.sender, "You are not the owner");
-        _transfer(msg.sender, to, tokenId);
+        safeTransferFrom(msg.sender, to, tokenId);
+        
+        _removeNFTFromOwner(msg.sender, tokenId);
+        
+        _addNFTToOwner(to, tokenId);
+    }
+
+    function _removeNFTFromOwner(address owner, uint256 tokenId) internal {
+        uint256[] storage tokens = ownerToTokens[owner];
+        uint256 index = _findTokenIndex(tokens, tokenId);
+
+        tokens[index] = tokens[tokens.length - 1];
+        tokens.pop();
+
+        string[] storage uris = ownerToTokenURI[owner];
+        uris[index] = uris[uris.length - 1];
+        uris.pop();
+    }
+
+    function _addNFTToOwner(address owner, uint256 tokenId) internal {
+        ownerToTokens[owner].push(tokenId);
+
+        string memory tokenUri = tokenURI(tokenId);
+        ownerToTokenURI[owner].push(tokenUri);
+    }
+
+    function _findTokenIndex(uint256[] storage tokens, uint256 tokenId) internal view returns (uint256) {
+        for (uint256 i = 0; i < tokens.length; i++) {
+            if (tokens[i] == tokenId) {
+                return i;
+            }
+        }
+        revert("Token not found");
     }
 
     function sendETH(address payable recipient) public payable {
@@ -38,4 +83,3 @@ contract NFT is ERC721URIStorage, Ownable {
         return address(this).balance;
     }
 }
-
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,19 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      chainId: 31337, 
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545", // Hardhat local network URL
+      // accounts: ["<your private key>"], 
+    },
+  },
+  etherscan: {
+    // You can add your API key for Etherscan if you're verifying contracts
+    // apiKey: "<your etherscan api key>",
+  },
 };
 
 export default config;
